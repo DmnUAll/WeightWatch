cae7586b579d8c8a9efdd446debb89c65ac67cf4
Ilya Valito <valito.spb@gmail.com>
2023-05-02 18:54:26 +0300

    Initial commit

---
+++ WeightWatch/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  WeightWatch
+//
+//  Created by Илья Валито on 02.05.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions
+    ) {
+//        guard let scene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: scene)
+//        window?.rootViewController = SplashViewController()
+//        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
